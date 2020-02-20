50915d3ffc7b29d6ea6310349ceecc9ca94c1fe0
pdarrell <dar17006@byui.edu>
2020-02-19 21:17:47 -0500

    lesson 7

---
+++ CIT230-Web/lesson-7/js/home.js
@@ -0,0 +1,45 @@
+/**
+ * JavaScript file for lazy load images
+ */
+
+function lazyload()
+{
+    const imgOptions = {
+        threshold: 1.0,
+        root: "main"
+    };
+
+
+    let imagesToLoad = document.querySelectorAll('img[data-src]');
+    const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+    };
+
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+
+    
+}
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
