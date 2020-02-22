2dcbd3085de431a6fdc0f5e0c3b0ab98732f4b2d
pdarrell <dar17006@byui.edu>
2020-02-21 23:25:29 -0500

    gallery

---
+++ CIT230-Web/gallery/js/home.js
@@ -0,0 +1,157 @@
+/**
+ * JavaScript file for Home Portal
+ */
+
+function toggleMenu()
+{
+    document.getElementById("primaryNav").classList.toggle("hide");
+
+}
+
+function contactCopyright()
+{
+    document.getElementById("copyright").innerHTML += "Purnell Darrell &bull; " + getCurrentDate();
+    displayBanner();
+}
+
+function getCurrentYear()
+{
+    var date = new Date();
+    var year = date.getFullYear();
+    return year;
+}
+
+function getCurrentDate()
+{
+    var date = new Date();
+    var month = findMonth(date.getMonth());
+    var day = date.getDate();
+    var year = date.getFullYear();
+    var dayString = findDay(date.getDay());
+    return (dayString + ", " + day + " " + month + " " + year);
+}
+
+function findMonth(monthNumber)
+{
+    switch (monthNumber)
+    {
+        case 0:
+            return "January";
+        case 1:
+            return "February"; 
+        case 2:
+            return "March"; 
+        case 3:
+            return "April";  
+        case 4:
+            return "May";
+        case 5:
+            return "June";
+        case 6:
+            return "July";
+        case 7:
+            return "August";
+        case 8:
+            return "September";
+        case 9:
+            return "October";
+        case 10:
+            return "November";
+        case 11:
+            return "December";
+        default:
+            return "";
+    }  
+}
+
+function findDay(dayNumber)
+{
+    switch (dayNumber)
+    {
+        case 0:
+            return "Sunday";
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
+        default:
+            return dayNumber;
+    }
+}
+
+function displayBanner()
+{
+    var date = new Date();
+    //check if Friday
+    if (date.getDay() == 5)
+    {
+        document.getElementById("banner").classList.add("showBanner");
+        document.getElementById("banner").classList.remove("hideBanner");
+    } else 
+    {
+        document.getElementById("banner").classList.add("hideBanner");
+        document.getElementById("banner").classList.remove("showBanner");
+    }
+
+}
+
+/*
+Lazy laoding with Intersetion Observer API
+*/
+
+
+    
+
+    if('IntersectionObserver' in window) {
+        document.addEventListener("scroll", observer, false);
+    
+        const imgOptions = {
+            threshold: 1.0,
+            root: document.querySelector('main')
+        };
+
+        let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+        const loadImages = (image) => {
+            image.setAttribute('src', image.getAttribute('data-src'));
+            image.onload = () => {
+                image.removeAttribute('data-src');
+            };
+        };
+
+        const observerCallback = (observer) => {
+            items.forEach((item) => {
+                if(item.isIntersecting) {
+                    loadImages(item.target);
+                    observer.unobserve(item.target);
+                }
+            });
+
+            imagesToLoad.forEach((img) => {
+                observer.observe(img);
+            });
+        }
+
+        const observer = new IntersectionObserver(items, observerCallback
+            , imgOptions); 
+         
+        
+    } else {
+        imagesToLoad.forEach((img) => {
+            loadImages(img);
+        });
+    }
+
+
+
+
+
+
